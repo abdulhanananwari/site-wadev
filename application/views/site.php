6e6904d3d3fb55c6ad1e3aa810170a7e7f9fde3b
abdulhanan <abdulhananx42@gmail.com>
2018-07-25 10:40:07 +0700

    first commit

---
+++ application/views/site.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--  Fonts and icons  -->
+      <!--     Fonts and icons     -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+  </head>
+  <body>
+      <nav class="navbar fixed-top navbar-expand-lg bg-info">
+          <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#service">Service</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#team">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portofolio">portofolio</a>
+                  </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/wadev.jpg')">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 ml-auto mr-auto">
+        <div class="brand text-center">
+          <!-- <h1>wadev solution</h1>
+          <h3 class="title text-center">Subtitle</h3> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title">Your main section here</h2>
+        <div class="tab-pane" id="service">
+            <div class="section text-center">
+                <div class="row">
+                  <div class="col-md-8 ml-auto mr-auto">
+                    <h2 class="title">Let&apos;s talk product</h2>
+                    <h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn&apos;t scroll to get here. Add a button if you want the user to see more.</h5>
+                  </div>
+                </div>
+                <div class="features">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="info">
+                        <div class="icon icon-info">
+                          <i class="material-icons">code</i>
+                        </div>
+                        <h4 class="info-title">Custom App</h4>
+                        <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="info">
+                        <div class="icon icon-success">
+                          <i class="material-icons">web</i>
+                        </div>
+                        <h4 class="info-title">Website</h4>
+                        <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="info">
+                        <div class="icon icon-danger">
+                          <i class="material-icons">computer</i>
+                        </div>
+                        <h4 class="info-title">Fingerprint</h4>
+                        <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        <div class="tab-pane" id="portofolio">
+          <div class="section text-center">
+              <h2>Portofolio</h2>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" alt="Card image cap">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+              
+          </div>
+        </div>
+        <div class="tab-pane" id="team">
+            <div class="section text-center">
+                <h2 class="title">Here is our team</h2>
+                <div class="team">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="team-player">
+                        <div class="card card-plain">
+                          <div class="col-md-6 ml-auto mr-auto">
+                            <img src="assets/img/faces/avatar.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                          </div>
+                          <h4 class="card-title">Gigi Hadid
+                            <br>
+                            <small class="card-description text-muted">Model</small>
+                          </h4>
+                          <div class="card-body">
+                            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                              <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                          </div>
+                          <div class="card-footer justify-content-center">
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="team-player">
+                        <div class="card card-plain">
+                          <div class="col-md-6 ml-auto mr-auto">
+                            <img src="assets/img/faces/christian.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                          </div>
+                          <h4 class="card-title">Christian Louboutin
+                            <br>
+                            <small class="card-description text-muted">Designer</small>
+                          </h4>
+                          <div class="card-body">
+                            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                              <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                          </div>
+                          <div class="card-footer justify-content-center">
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-linkedin"></i></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="team-player">
+                        <div class="card card-plain">
+                          <div class="col-md-6 ml-auto mr-auto">
+                            <img src="assets/img/faces/kendall.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                          </div>
+                          <h4 class="card-title">Kendall Jenner
+                            <br>
+                            <small class="card-description text-muted">Model</small>
+                          </h4>
+                          <div class="card-body">
+                            <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                              <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                          </div>
+                          <div class="card-footer justify-content-center">
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                            <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<footer class="footer footer-default" >
+  <div class="container">
+    <nav class="float-left">
+      <ul>
+        <li>
+          <a href="https://wadev.com/">
+              wadev developer team
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="https://www.creative-tim.com/" target="blank">Creative Tim</a> for a better web.
+    </div>
+  </div>
+</footer>
+<!--   Core JS Files   -->
+<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Google Maps Plugin  -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script></body>
+</html>
+</html>
